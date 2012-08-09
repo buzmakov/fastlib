d171dbbdd3dde74cba35f6468cc1acd8d8fa54f7
Alexey Buzmakov <buzmakov@gmail.com>
2012-08-09 23:20:21 +0400

    add fast volume rotation via intel ispc modify Makefile

--- fastlib/imageprocessing/ispmd/ispmd_image_processing.pyx
+++ fastlib/imageprocessing/ispmd/ispmd_image_processing.pyx
@@ -9,6 +9,7 @@ cimport cython
 
 cdef extern from "C/objs/image_processing_ispc.h":
     void rotate_mt(float* data, float* res, float angle, int im_size)
+    void rotate_volume(float* data, float* res, float angle, int im_size, int slices_count)
     void backproject(float* vin, float* vout, int im_size)
     void project(float* vin, float* vout, int im_size)
     void add_mt(float* v1, float* v2, int im_size)
@@ -64,3 +65,12 @@ def add_fast_ref(numpy.ndarray[numpy.float32_t, ndim=2] v1, numpy.ndarray[numpy.
 #    TODO: chek shape(v1)=shape(v2)
     cdef numpy.int32_t count=v1.shape[0]*v1.shape[1]
     add_mt(<float *>v1.data,<float *> v2.data,count)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def rotate_volume_fast(numpy.ndarray[numpy.float32_t, ndim=3] data, numpy.float32_t angle):
+    cdef numpy.int32_t im_size=data.shape[1]
+    cdef numpy.int32_t slice_count=data.shape[0]
+    cdef numpy.ndarray[numpy.float32_t, ndim=3] res=numpy.zeros_like(data)
+    rotate_volume(<float *> data.data, <float *> res.data, angle, im_size,slice_count)
+    return res
